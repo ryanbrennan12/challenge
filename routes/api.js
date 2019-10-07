bbeb5798db3e1f7de8e0eb3adeb6dcc0c6a381b9
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-10-07 00:22:29 -0700

    Cleanup server.js

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,10 @@
 const path = require('path')
 const express = require('express');
 const router = express.Router();
-const utils = require('../../utils');
+const utils = require('../utils/index.js');
 
 router.get('/all', (req, res) => {
-console.log('router.get/all')
   utils.getAll((error, response) => {
-    console.log('I am the word', response.data)
     res.status(200).json(response.data);
   })
 })
